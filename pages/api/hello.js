6e33c0bc98a474fb24c159ba1b7c6cc4d5316e2b
shaundee <shawndeewilson@gmail.com>
2022-03-24 12:50:12 +0000

    final build

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,13 +1,3 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-// const API_KEY = process.env.API_KEY
-//  export const url = `https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`
-
- 
-// export default async function handler(req, res) {
-  
- 
-//   const data = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`).then(response => response.json())
-
-//   res.json(data);
- 
-// }
+export default function handler(req, res) {
+  res.status(200).json({ name: "John Doe" });
+}
